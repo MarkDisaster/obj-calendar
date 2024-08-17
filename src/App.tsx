025d0ad9cdf8f006a2d0bf493187b5c3828bf743
Marek Marek <marekmarek@Marek-MacBook-Pro.local>
2024-08-17 15:26:51 +0200

    build test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { createSignal } from 'solid-js';
 import './App.css';
 
 function App() {
-   const [count, setCount] = createSignal(47);
+   const [count] = createSignal(47);
 
    return <>Hello Vite {count()}</>;
 }
